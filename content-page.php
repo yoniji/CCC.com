a40c4f9780be1aade39f32024c31407e1438bd52
Yoniji <hello.hyu@gmail.com>
2012-02-29 14:50:44 +0800

    front-page

--- content-page.php
+++ content-page.php
@@ -3,21 +3,16 @@
  * The template used for displaying page content in page.php
  *
  * @package WordPress
- * @subpackage Twenty_Eleven
- * @since Twenty Eleven 1.0
+ * @subpackage chirp
  */
 ?>
 
 <div id="post-<?php echo($post->post_name); ?>" <?php post_class(); ?>>
-	<div class="entry-header">
-		<h1 class="<?php echo((empty( $post->post_parent ))?"parent-title":"sub-page-title entry-title");?>"><span><?php echo((empty( $post->post_parent ))?"&gt;&gt;":"");?> </span><?php the_title(); ?></h1>
-	</div><!-- .entry-header -->
-
-	<div class="entry-content">
+	<div class="entry-content page-content">
 		<?php the_content(); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'chirp' ) . '</span>', 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
 	<div class="entry-meta">
 		<?php edit_post_link( __( 'Edit', 'chirp' ), '<span class="edit-link">', '</span>' ); ?>
 	</div><!-- .entry-meta -->
 </div><!-- #post-<?php the_ID(); ?> -->
+
--- page.php
+++ page.php
@@ -12,11 +12,6 @@ get_header(); ?>
 			<div id="content" role="main">
 
 				<?php the_post(); ?>
-                
-                <?php if( !empty( $post->post_parent )) { ?> 
-                <div class="parent-title"><a href="<?php echo(get_permalink($post->post_parent)); ?>">&gt;&gt; <?php echo(get_the_title( $post->post_parent )); ?></a></div>
-				<?php } ?>
-
 				<?php get_template_part( 'content', 'page' ); ?>
 
 			</div><!-- #content -->
